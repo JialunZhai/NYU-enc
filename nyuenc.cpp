8f688e6e2d8a14c23bf6e6229784480434da72f8
Jialun Zhai <jlzhai@ustc.edu>
2022-04-12 07:23:34 -0400

    add error msg

--- nyuenc.cpp
+++ nyuenc.cpp
@@ -23,6 +23,9 @@ using namespace std;
 
 class Task{
 public: 
+    Task() = default;
+    Task(const Task&) = delete;
+    Task& operator=(const Task&) = delete;
     virtual ~Task() = default;
     virtual void run() = 0;
 };
@@ -36,6 +39,9 @@ public:
 class ThreadPool{
 public: 
     ThreadPool(const unsigned &pool_sz);
+    ThreadPool(const ThreadPool&) = delete;
+    ~ThreadPool();
+    ThreadPool& operator=(const ThreadPool&) = delete;
     ThreadPool& addTask(unique_ptr<Task> task);
     void shutDown();
     size_t waitingQueueSize()const;
@@ -53,28 +59,26 @@ ThreadPool::ThreadPool(const unsigned &pool_sz):shutdown(false),mutex(PTHREAD_MU
     //set detach
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED)){
-        cerr<<"Error in ThreadPool: set detach failed"<<endl;
-        exit(EXIT_FAILURE);
-    }
-    if(pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM)){
-        cerr<<"Error in ThreadPool: set scope failed"<<endl;
-        exit(EXIT_FAILURE);
-    }
+    if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED))
+        throw runtime_error("set detach failed");
+    if(pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM)) 
+        throw runtime_error("set scope failed");
     //create work threads
     for(unsigned i=0;i!=pool_sz;++i){
         pthread_t tid;
-        if(pthread_create(&tid,&attr,&ThreadPool::workThread,(void*)this)){
-            cerr<<"Error in ThreadPool: pthread_create failed"<<endl;
-            exit(EXIT_FAILURE);
-        }
+        if(pthread_create(&tid,&attr,&ThreadPool::workThread,(void*)this))
+            throw runtime_error("create pthread failed");
     }
+}
+
+ThreadPool::~ThreadPool(){
+    shutdown=true;
 }
 
 void* ThreadPool::workThread(void* which_pool){
     auto pool=static_cast<ThreadPool*>(which_pool);
     pool->worker();
-    return NULL;
+    return nullptr;
 }
 
 void ThreadPool::worker(){
@@ -144,7 +148,7 @@ public:
         while(++i<collector[chunk_id].second){
             if(src[i]==pre_ch) ++cnt;
             else{
-                if(pre_ch==EOF||cnt==EOF) cerr<<"Error: EOF comes before the end of file"<<endl;
+                if(pre_ch==EOF||cnt==EOF) throw runtime_error("EOF comes before the end of file");
                 dst[j++]=pre_ch;
                 dst[j++]=static_cast<unsigned char>(cnt);
                 pre_ch=src[i];
@@ -183,115 +187,104 @@ void parse_cmd(int argc, char* argv[],unsigned &worker_num,char** &ptr_infile) {
 
 
 int main(int argc, char* argv[]){
-    chunks=(char*)malloc(CHUNKS_SZ);
-    if(!chunks){
-        cerr<<"ERROR: malloc chunks failed"<<endl;
-        exit(EXIT_FAILURE);
-    }
+    try{
+        // initialize
+        if(!(chunks=(char*)malloc(CHUNKS_SZ))) 
+            throw runtime_error("malloc chunks failed");
+        unsigned worker_num=1;
+        char** ptr_infile=nullptr;
+        parse_cmd(argc,argv,worker_num,ptr_infile);    
+        vector<int> fd_in_lst;
+        vector<pair<char*,unsigned long>> mmap_src_lst;
+        unsigned chunk_num=0;
 
-    unsigned worker_num=1;
-    char** ptr_infile=nullptr;
-    parse_cmd(argc,argv,worker_num,ptr_infile);
-    ThreadPool pool(worker_num);
+        // dispatch works
+        ThreadPool pool(worker_num);
+        while(*ptr_infile!=nullptr){
+            char* infile=*ptr_infile;
+            int fd_in=open(infile,O_RDONLY);
+            if(fd_in==-1) throw runtime_error("open infile failed");
+            fd_in_lst.push_back(fd_in);
+            
+            struct stat stat_infile;
+            stat(infile,&stat_infile);
+            unsigned long file_sz=stat_infile.st_size;
 
-    vector<int> fd_in_lst;
-    vector<pair<char*,unsigned long>> mmap_src_lst;
+            char* src_bg=nullptr;
+            if(!(src_bg=(char*)mmap(nullptr,file_sz,PROT_READ,MAP_PRIVATE,fd_in,0))) 
+                throw runtime_error("mmap input-file failed");
+            mmap_src_lst.push_back({src_bg,file_sz});
 
-    unsigned chunk_num=0;
-    while(*ptr_infile!=nullptr){
-        char* infile=*ptr_infile;
-        int fd_in=open(infile,O_RDONLY);
-        if(fd_in==-1){
-            cerr<<"ERROR: open infile failed"<<endl;
-            exit(EXIT_FAILURE);
-        }
-        fd_in_lst.push_back(fd_in);
-        
-        struct stat stat_infile;
-        stat(infile,&stat_infile);
-        unsigned long file_sz=stat_infile.st_size;
-
-        char* src_bg=nullptr;
-        if(!(src_bg=(char*)mmap(NULL,file_sz,PROT_READ,MAP_PRIVATE,fd_in,0))){
-            cerr<<"ERROR: mmap input-file failed: ";
-            perror(strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-        mmap_src_lst.push_back({src_bg,file_sz});
-
-        for(unsigned long start=0;start<file_sz;start+=CHUNK_SZ){
-            flags[chunk_num]=false;
-            unsigned len=(start+CHUNK_SZ>file_sz)?file_sz-start:CHUNK_SZ;
-            collector[chunk_num]={nullptr,len};
-            //----------------- critical region begin ----------------------------
-            //pthread_mutex_lock(&flags_mutex);
-            //while(waiting_queue_size>=WAITING_QUEUE_MAX_SZ) pthread_cond_wait(&flags_cond, &flags_mutex);
-            //++waiting_queue_size;
-            //pthread_mutex_unlock(&flags_mutex);
-            //----------------- critical region end ------------------------------
-            try{
+            for(unsigned long start=0;start<file_sz;start+=CHUNK_SZ){
+                flags[chunk_num]=false;
+                unsigned len=(start+CHUNK_SZ>file_sz)?file_sz-start:CHUNK_SZ;
+                collector[chunk_num]={nullptr,len};
+                //----------------- critical region begin ----------------------------
+                //pthread_mutex_lock(&flags_mutex);
+                //while(waiting_queue_size>=WAITING_QUEUE_MAX_SZ) pthread_cond_wait(&flags_cond, &flags_mutex);
+                //++waiting_queue_size;
+                //pthread_mutex_unlock(&flags_mutex);
+                //----------------- critical region end ------------------------------
                 unique_ptr<Encoder> encoder(new Encoder(src_bg+start,chunk_num++));
                 pool.addTask(std::move(encoder));
-            }catch(exception &e){
-                cerr<<"ERROR: add new task failed: "<<endl;
-                cerr<<e.what()<<endl;
-                exit(EXIT_FAILURE);
             }
+            ++ptr_infile;
         }
-        ++ptr_infile;
-    }
+        pool.shutDown();
 
-    pool.shutDown();
-
-    if(chunk_num!=0){
-        //----------------- critical region begin ----------------------------
-        pthread_mutex_lock(&flags_mutex);
-        while(flags[0]==false) pthread_cond_wait(&flags_cond, &flags_mutex);
-        flags[0]=false;
-        pthread_mutex_unlock(&flags_mutex);
-        //----------------- critical region end ------------------------------
-        if(chunk_num==1){
-            fwrite(collector[0].first,1,collector[0].second,stdout);
-        }
-        else{
-            char* chunk=collector[0].first;
-            unsigned len=collector[0].second;
-            char pre_c=chunk[len-2];
-            unsigned char pre_cnt=chunk[len-1];
-            fwrite(chunk,1,len-2,stdout);
-            for(unsigned i=1;i<chunk_num;++i){
-                //----------------- critical region begin ----------------------------
-                pthread_mutex_lock(&flags_mutex);
-                while(flags[i]==false) pthread_cond_wait(&flags_cond, &flags_mutex);
-                flags[i]=false;
-                pthread_mutex_unlock(&flags_mutex);
-                //----------------- critical region end ------------------------------
-                chunk=collector[i].first;
-                len=collector[i].second;
-                if(pre_c==chunk[0]){
-                    unsigned char cnt=static_cast<unsigned>(pre_cnt)+static_cast<unsigned>(chunk[1]);
-                    if(len>=4){
+        // merge chunks
+        if(chunk_num!=0){
+            //----------------- critical region begin ----------------------------
+            pthread_mutex_lock(&flags_mutex);
+            while(flags[0]==false) pthread_cond_wait(&flags_cond, &flags_mutex);
+            flags[0]=false;
+            pthread_mutex_unlock(&flags_mutex);
+            //----------------- critical region end ------------------------------
+            if(chunk_num==1){
+                fwrite(collector[0].first,1,collector[0].second,stdout);
+            }
+            else{
+                char* chunk=collector[0].first;
+                unsigned len=collector[0].second;
+                char pre_c=chunk[len-2];
+                unsigned char pre_cnt=chunk[len-1];
+                fwrite(chunk,1,len-2,stdout);
+                for(unsigned i=1;i<chunk_num;++i){
+                    //----------------- critical region begin ----------------------------
+                    pthread_mutex_lock(&flags_mutex);
+                    while(flags[i]==false) pthread_cond_wait(&flags_cond, &flags_mutex);
+                    flags[i]=false;
+                    pthread_mutex_unlock(&flags_mutex);
+                    //----------------- critical region end ------------------------------
+                    chunk=collector[i].first;
+                    len=collector[i].second;
+                    if(pre_c==chunk[0]){
+                        unsigned char cnt=static_cast<unsigned>(pre_cnt)+static_cast<unsigned>(chunk[1]);
+                        if(len>=4){
+                            fwrite(&pre_c,1,1,stdout);
+                            fwrite(&cnt,1,1,stdout);
+                            fwrite(chunk+2,1,len-4,stdout);
+                            pre_c=chunk[len-2];
+                            pre_cnt=chunk[len-1];
+                        }
+                        else pre_cnt=cnt;// len==2
+                    }
+                    else{
                         fwrite(&pre_c,1,1,stdout);
-                        fwrite(&cnt,1,1,stdout);
-                        fwrite(chunk+2,1,len-4,stdout);
+                        fwrite(&pre_cnt,1,1,stdout);
+                        if(len>2) fwrite(chunk,1,len-2,stdout);
                         pre_c=chunk[len-2];
                         pre_cnt=chunk[len-1];
                     }
-                    else pre_cnt=cnt;// len==2
                 }
-                else{
-                    fwrite(&pre_c,1,1,stdout);
-                    fwrite(&pre_cnt,1,1,stdout);
-                    if(len>2) fwrite(chunk,1,len-2,stdout);
-                    pre_c=chunk[len-2];
-                    pre_cnt=chunk[len-1];
-                }
+                fwrite(&pre_c,1,1,stdout);
+                fwrite(&pre_cnt,1,1,stdout);
             }
-            fwrite(&pre_c,1,1,stdout);
-            fwrite(&pre_cnt,1,1,stdout);
         }
+        for(auto &src_sz:mmap_src_lst) munmap(src_sz.first,src_sz.second);
+        for(auto &fd_in:fd_in_lst) close(fd_in);
+        free(chunks);
+    }catch(runtime_error& err){
+        cerr<<err.what()<<endl;
     }
-    for(auto &src_sz:mmap_src_lst) munmap(src_sz.first,src_sz.second);
-    for(auto &fd_in:fd_in_lst) close(fd_in);
-    free(chunks);
 }
